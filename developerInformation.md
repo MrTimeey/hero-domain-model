0ff505e226ddcdac218e4b4cd76462cba75ef051
Kruse Tim <t.kruse@reply.de>
2023-11-21 16:07:33 +0100

    Dependency update

---
+++ developerInformation.md
@@ -0,0 +1,12 @@
+# hero-domain-model
+
+## Dependency updates
+Show dependency updates:
+```shell
+ mvn versions:display-dependency-updates
+```
+
+Automatically update dependencies:
+```shell
+mvn versions:update-properties
+```
--- src/main/java/io/github/mrtimeey/herodomainmodel/core/DataLoader.java
+++ src/main/java/io/github/mrtimeey/herodomainmodel/core/DataLoader.java
@@ -2,6 +2,7 @@ package io.github.mrtimeey.herodomainmodel.core;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import io.github.mrtimeey.herodomainmodel.model.SuperHero;
+import io.github.sfrick.jcontrols.Try;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
@@ -16,14 +17,17 @@ final class DataLoader {
    }
 
    static List<SuperHero> getAllHeroes() {
-      try (InputStream resource = DataLoader.class.getResourceAsStream("heroes.json")) {
-         String inputObject = IOUtils.toString(resource, StandardCharsets.UTF_8);
-         TypeReference<List<SuperHero>> typeReference = new TypeReference<>() {
-         };
-         return ObjectConversionUtils.toObject(inputObject, typeReference).orElse(List.of());
-      } catch (IOException e) {
-         throw new RuntimeException(e);
-      }
+      return Try
+            .withResource(() -> DataLoader.class.getResourceAsStream("heroes.json"))
+            .of(DataLoader::parseInputStream)
+            .orElse(List.of());
+   }
+
+   private static List<SuperHero> parseInputStream(InputStream resource) throws IOException {
+      String inputObject = IOUtils.toString(resource, StandardCharsets.UTF_8);
+      TypeReference<List<SuperHero>> typeReference = new TypeReference<>() {
+      };
+      return ObjectConversionUtils.toObject(inputObject, typeReference).orElse(List.of());
    }
 
 }
